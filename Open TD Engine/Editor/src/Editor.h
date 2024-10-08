87fb5e6f1c8e0eba28b0faa51a0693f1ef833c5c
Charlotte <cmjwfeltwell@gmail.com>
2024-10-08 10:35:43 -0500

    Update Editor.h

--- Open TD Engine/Editor/src/Editor.h
+++ Open TD Engine/Editor/src/Editor.h
@@ -2,19 +2,32 @@
 #define EDITOR_H
 
 #include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+#include <windows.h>
 
 class Editor {
 public:
 	Editor();
 	~Editor();
 
+	void run();
+
 private:
 	void initialize();
 	void initMenubar();
+	void render();
+	
+	void cleanup();
 
 	const int EDITOR_WIDTH = 1200;
 	const int EDITOR_HEIGHT = 1000;
 	sf::RenderWindow window;
+	
+	sf::Clock deltaClock;
+
+	bool isDarkMode;
 };
 
 #endif
