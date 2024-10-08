1fd26e4e5afab45686c9800aaea152be81efea07
Charlotte <cmjwfeltwell@gmail.com>
2024-10-08 10:21:23 -0500

    Create Editor.h

--- Open TD Engine/Editor/src/Editor.h
+++ Open TD Engine/Editor/src/Editor.h
@@ -1,4 +1,20 @@
-#pragma once
+#ifndef EDITOR_H
+#define EDITOR_H
 
-#define EDITOR_WIDTH 1200
-#define EDITOR_HEIGHT 1000
+#include <SFML/Graphics.hpp>
+
+class Editor {
+public:
+	Editor();
+	~Editor();
+
+private:
+	void initialize();
+	void initMenubar();
+
+	const int EDITOR_WIDTH = 1200;
+	const int EDITOR_HEIGHT = 1000;
+	sf::RenderWindow window;
+};
+
+#endif
