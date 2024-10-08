a8fd65f76e13859a507a322a4ceee91675044dbc
Charlotte <cmjwfeltwell@gmail.com>
2024-10-08 15:28:57 -0500

    Update Player.cpp

--- Open TD Engine/Engine/src/Player.cpp
+++ Open TD Engine/Engine/src/Player.cpp
@@ -11,11 +11,11 @@ Player::~Player() {
 }
 
 void Player::initialize() {
-	player.setSize(sf::Vector2f(50.f, 50.f));  // 50x50 pixel square
-	player.setFillColor(sf::Color::Blue);     // Set the color of the shape
-	player.setPosition(GAME_WIDTH / 2, GAME_HEIGHT / 2);  // Start in the center of the
+	sprite.setSize(sf::Vector2f(50.f, 50.f));  // 50x50 pixel square
+	sprite.setFillColor(sf::Color::Blue);     // Set the color of the shape
+	sprite.setPosition(800 / 2, 600 / 2);  // Start in the center of the
 }
 
 void Player::draw(sf::RenderWindow& window) {
-
+	window.draw(sprite);
 }
