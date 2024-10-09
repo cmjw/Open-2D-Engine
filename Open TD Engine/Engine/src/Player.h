bdd54d12dd2703b47863be9a6fd8496f1fc5014b
Charlotte <cmjwfeltwell@gmail.com>
2024-10-08 19:07:05 -0500

    Add move function

--- Open TD Engine/Engine/src/Player.h
+++ Open TD Engine/Engine/src/Player.h
@@ -24,6 +24,8 @@ public:
 
 	void draw(sf::RenderWindow& window);
 
+	void move(float dx, float dy, float deltaTime);
+
 private:
 	void loadPlayerData();
 	void initialize(); // loads and initializes data
