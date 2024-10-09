c81718f411925e6b9d2581a46aa8fa9847afce18
Charlotte <cmjwfeltwell@gmail.com>
2024-10-08 19:06:53 -0500

    Add move function

--- Open TD Engine/Engine/src/Player.cpp
+++ Open TD Engine/Engine/src/Player.cpp
@@ -13,9 +13,15 @@ Player::~Player() {
 void Player::initialize() {
 	sprite.setSize(sf::Vector2f(50.f, 50.f));  // 50x50 pixel square
 	sprite.setFillColor(sf::Color::Blue);     // Set the color of the shape
-	sprite.setPosition(800 / 2, 600 / 2);  // Start in the center of the
+	sprite.setPosition(0, 0);  // Start in the center of the
 }
 
 void Player::draw(sf::RenderWindow& window) {
 	window.draw(sprite);
+}
+
+void Player::move(float dx, float dy, float deltaTime) {
+	float movementSpeed = 200.f;
+
+	sprite.move(dx * movementSpeed * deltaTime, dy * movementSpeed * deltaTime);
 }
