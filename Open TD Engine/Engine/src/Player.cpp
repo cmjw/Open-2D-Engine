1065fc534e569d0e9b14847294e472f403b05f9f
Charlotte <cmjwfeltwell@gmail.com>
2024-10-09 09:48:22 -0500

    Create loadPlayerData()

--- Open TD Engine/Engine/src/Player.cpp
+++ Open TD Engine/Engine/src/Player.cpp
@@ -11,6 +11,8 @@ Player::~Player() {
 }
 
 void Player::initialize() {
+	loadPlayerData();
+
 	sprite.setSize(sf::Vector2f(50.f, 50.f)); 
 	sprite.setFillColor(sf::Color::Blue);    
 	sprite.setPosition(0, 0); 
@@ -18,15 +20,33 @@ void Player::initialize() {
 	movementSpeed = 200.f;
 }
 
+void Player::loadPlayerData() {
+	// eventually this is saved as some "starting graphic"
+
+	std::string jsonFile = "../Data/Player.json";
+
+	std::ifstream file(jsonFile);
+
+	if (!file.is_open()) {
+		std::cerr << "Could not open file: " << jsonFile << std::endl;
+		return;
+	}
+	/*nlohmann::json playerData;
+	file >> playerData;*/
+
+	//width = playerData["size"]["width"];
+	//height = playerData["size"]["height"];
+}
+
 void Player::draw(sf::RenderWindow& window) {
 	window.draw(sprite);
 }
 
 void Player::move(float dx, float dy, float deltaTime) {
-	float newDx = dx * movementSpeed * deltaTime;
-	float newDy = dy * movementSpeed * deltaTime;
+	float newX = dx * movementSpeed * deltaTime;
+	float newY = dy * movementSpeed * deltaTime;
 
 	// collisions?
 
-	sprite.move(newDx, newDy);
+	sprite.move(newX, newY);
 }
