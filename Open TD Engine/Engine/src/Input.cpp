e395cb41d1b17c471dd8c0ac23e66ddf9ac1ce30
Charlotte <cmjwfeltwell@gmail.com>
2024-10-12 14:51:01 -0500

    Add keypress handlers

--- Open TD Engine/Engine/src/Input.cpp
+++ Open TD Engine/Engine/src/Input.cpp
@@ -4,10 +4,23 @@ Input::Input() {
 
 }
 
-Input::Input(sf::Keyboard keyboard) {
-	this->keyboard = keyboard;
-}
-
 Input::~Input() {
 
+}
+
+bool Input::upPressed() {
+	return (sf::Keyboard::isKeyPressed(sf::Keyboard::Up));
+	// this will be a constant eventually
+}
+
+bool Input::downPressed() {
+	return (sf::Keyboard::isKeyPressed(sf::Keyboard::Down));
+}
+
+bool Input::leftPressed() {
+	return (sf::Keyboard::isKeyPressed(sf::Keyboard::Left));
+}
+
+bool Input::rightPressed() {
+	return (sf::Keyboard::isKeyPressed(sf::Keyboard::Right));
 }
