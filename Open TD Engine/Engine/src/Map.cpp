0552c9db9ac16f729acb56ca2b84b06618332001
Charlotte <cmjwfeltwell@gmail.com>
2024-10-12 16:44:21 -0500

    Update Map.cpp

--- Open TD Engine/Engine/src/Map.cpp
+++ Open TD Engine/Engine/src/Map.cpp
@@ -1,2 +1,17 @@
 #include "Map.h"
 
+Map::Map() {
+
+}
+
+Map::Map(int width, int height, const std::string& tilesetPath) {
+
+}
+
+int Map::getWidth() {
+	return mapWidth;
+}
+
+int Map::getHeight() {
+	return mapHeight;
+}
